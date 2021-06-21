234dbe04f9d4105d94c878b3c490008d34ab5d16
coco-sha <hellococo234@gmail.com>
2021-06-21 19:02:01 +0530

    add init

---
+++ packages/polar/src/builtin-tasks/init.ts
@@ -0,0 +1,10 @@
+import { task } from "../internal/core/config/config-env";
+import { TASK_INIT } from "./task-names";
+
+export default function (): void {
+  task(TASK_INIT, "Initializes a new project in the given directory")
+    .addPositionalParam<string>("newProjectLocation", "Location of the new project")
+    .setAction(async ({ newProjectLocation }: { newProjectLocation: string }, _) => {
+      // write init here
+    });
+}

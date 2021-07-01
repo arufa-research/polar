0633ffb169e2d3c61fa783cd0ae30d764c0b5950
udit-gulati <uditgulati6@gmail.com>
2021-07-01 15:01:29 +0530

    update polar config file

--- examples/sample-example/polar.config.js
+++ examples/sample-example/polar.config.js
@@ -1,7 +1,15 @@
-export default {
+module.exports = {
   defaultNetwork: 'development',
   networks: {
     development: {
+      endpoint: 'tcp://0.0.0.0:26656',
+      nodeId: '115aa0a629f5d70dd1d464bc7e42799e00f4edae',
+      chainId: 'enigma-pub-testnet-3',
+      trustNode: true,
+      keyringBackend: 'test',
+      types: {}
+    },
+    default: {
       endpoint: 'tcp://0.0.0.0:26656',
       nodeId: '115aa0a629f5d70dd1d464bc7e42799e00f4edae',
       chainId: 'enigma-pub-testnet-3',

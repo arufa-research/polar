0b9bdfb15fce5dd2fb4010c020f117399913dd02
udit-gulati <uditgulati6@gmail.com>
2021-07-20 01:35:27 +0530

    add deploy task to cli

---
+++ packages/polar/src/builtin-tasks/deploy.ts
@@ -0,0 +1,23 @@
+import chalk from "chalk";
+import fsExtra from "fs-extra";
+import path from "path";
+
+import { task } from "../internal/core/config/config-env";
+import { isCwdProjectDir } from "../internal/core/project-structure";
+import type { PolarRuntimeEnvironment } from "../types";
+import { TASK_DEPLOY } from "./task-names";
+
+export default function (): void {
+  task(TASK_DEPLOY, "Compresses the contracts files and deploys them to the network specified")
+    .setAction(async (env: PolarRuntimeEnvironment) => {
+      if (!isCwdProjectDir()) {
+        console.log(`Not in a valid polar project repo, exiting`);
+        process.exit(1);
+      }
+
+      // generated compressed wasm file using docker image
+      // choose image based on single or multi contact repo
+
+      // store compress wasm in secret network container
+    });
+}

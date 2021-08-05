2ae0ab6adb7bd69c0d1cb90a728d85f5face7e96
coco-sha <hellococo234@gmail.com>
2021-08-06 02:36:15 +0530

    add support for multiple contracts

--- packages/polar/src/lib/compile/compile.ts
+++ packages/polar/src/lib/compile/compile.ts
@@ -33,25 +33,25 @@ export async function compile (
     }
   } else {
     const paths = readdirSync(CONTRACTS_DIR);
-
     // Only one contract in the contracts dir and compile in contracts dir only
     if (paths.includes("Cargo.toml")) {
       contractDirs.push(CONTRACTS_DIR);
-    }
-
-    // Multiple contracts and each should be compiled by going inside each of them
-    for (const p of paths.sort(cmpStr)) {
-      const f = path.basename(p);
-      const contractAbsPath = path.resolve(CONTRACTS_DIR, f);
-      contractDirs.push(contractAbsPath);
+    } else {
+      // Multiple contracts and each should be compiled by going inside each of them
+      for (const p of paths.sort(cmpStr)) {
+        const f = path.basename(p);
+        const contractAbsPath = path.resolve(CONTRACTS_DIR, f);
+        contractDirs.push(contractAbsPath);
+      }
     }
   }
 
   for (const dir of contractDirs) {
     compileContract(dir, docker);
-    // generateSchema(dir, docker);
+    createArtifacts(
+      path.join(dir, TARGET_DIR), SCHEMA_DIR, path.join(ARTIFACTS_DIR, dir), docker
+    );
   }
-  createArtifacts(TARGET_DIR, SCHEMA_DIR, ARTIFACTS_DIR, docker);
 }
 
 export function compileContract (contractDir: string, docker: boolean): void {
@@ -91,21 +91,26 @@ export function createArtifacts (
 
     console.log(`Copying file ${filename} from ${chalk.gray(targetDir)} to ${chalk.gray(artifactsDir)}`);
     const sourcePath = path.resolve(targetDir, filename);
+    // create nested dirs if not present
+    if (!fs.existsSync(artifactsDir)) {
+      fs.mkdirSync(artifactsDir, { recursive: true });
+    }
     const destPath = path.resolve(artifactsDir, filename);
     fs.copyFileSync(sourcePath, destPath);
   }
 
-  const schemaPaths = fs.readdirSync(schemaDir);
+  // const schemaPaths = fs.readdirSync(schemaDir);
 
-  for (const p of schemaPaths) {
+  /* for (const p of schemaPaths) {
     const filename = path.basename(p);
     if (filename.split('.')[filename.split('.').length - 1] !== "json") {
       continue;
     }
 
-    console.log(`Copying file ${filename} from ${chalk.gray(schemaDir)} to ${chalk.gray(artifactsDir)}`);
+    console.log(
+      `Copying file ${filename} from ${chalk.gray(schemaDir)}to ${chalk.gray(artifactsDir)}`);
     const sourcePath = path.resolve(schemaDir, filename);
     const destPath = path.resolve(artifactsDir, filename);
     fs.copyFileSync(sourcePath, destPath);
-  }
+  } */
 }

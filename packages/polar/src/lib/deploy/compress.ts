44db2ebf3da9a76a6da73fc1ab03ebfaa3fabc0d
udit-gulati <uditgulati6@gmail.com>
2021-08-15 17:36:46 +0530

    move docker image versions constants

--- packages/polar/src/lib/deploy/compress.ts
+++ packages/polar/src/lib/deploy/compress.ts
@@ -8,7 +8,9 @@ import {
   ARTIFACTS_DIR,
   assertDir,
   CONTRACTS_DIR,
-  CONTRACTS_OUT_DIR
+  CONTRACTS_OUT_DIR,
+  multiImageVersion,
+  singleImageVersion
 } from "../../internal/core/project-structure";
 import { cmpStr } from "../../internal/util/strings";
 import type { PolarRuntimeEnvironment } from "../../types";
@@ -19,13 +21,12 @@ export async function compress (
 ): Promise<void> {
   const paths = readdirSync(CONTRACTS_DIR);
   if (paths.includes("Cargo.toml")) { // Only one contract in the contracts dir and compile in contracts dir only
-    const imageVersion = `0.11.5`;
     const dockerCmd = `docker run --rm -v ${path.resolve(CONTRACTS_DIR)}:/code \
                   --mount type=volume,source=${path.basename(ARTIFACTS_DIR)},target=/code/target \
                   --mount type=volume,source=${path.basename(ARTIFACTS_DIR)},target=/usr/local/cargo/registry \
-                  cosmwasm/rust-optimizer:${imageVersion}`;
+                  cosmwasm/rust-optimizer:${singleImageVersion}`;
 
-    console.log(chalk.greenBright(`Creating compressed .wasm file using cosmwasm/rust-optimizer:${imageVersion}...`));
+    console.log(chalk.greenBright(`Creating compressed .wasm file using cosmwasm/rust-optimizer:${singleImageVersion}...`));
     execSync(dockerCmd, { stdio: 'inherit' });
 
     const fileContents = fs.createReadStream(path.join(CONTRACTS_DIR, "contract.wasm.gz"));
@@ -36,13 +37,12 @@ export async function compress (
     fs.unlinkSync(path.join(CONTRACTS_DIR, "contract.wasm.gz"));
     console.log(`Created file ${path.join(CONTRACTS_OUT_DIR, "contract.wasm")}`);
   } else { // Multiple contracts and each should be compiled by going inside each of them
-    const imageVersion = `0.11.5`;
     const dockerCmd = `docker run --rm -v ${path.resolve(CONTRACTS_DIR)}:/code \
                   --mount type=volume,source=${path.basename(ARTIFACTS_DIR)},target=/code/target \
                   --mount type=volume,source=${path.basename(ARTIFACTS_DIR)},target=/usr/local/cargo/registry \
-                  cosmwasm/workspace-optimizer:${imageVersion}`;
+                  cosmwasm/workspace-optimizer:${multiImageVersion}`;
 
-    console.log(chalk.greenBright(`Creating compressed .wasm file using cosmwasm/workspace-optimizer:${imageVersion}...`));
+    console.log(chalk.greenBright(`Creating compressed .wasm file using cosmwasm/workspace-optimizer:${multiImageVersion}...`));
     execSync(dockerCmd, { stdio: 'inherit' });
     console.log(`Generated .wasm files in ${ARTIFACTS_DIR}`);
   }

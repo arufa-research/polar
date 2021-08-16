762803fb634c4c180b2e6b09cce6237e46c3ea79
coco-sha <hellococo234@gmail.com>
2021-08-16 07:47:42 +0530

    add semicolon

--- packages/polar/test/fixture-projects/config-extensions/polar.config.js
+++ packages/polar/test/fixture-projects/config-extensions/polar.config.js
@@ -2,10 +2,10 @@ import { extendConfig } from '../../../src/internal/core/config/config-env'
 
 extendConfig((config, _userConfig) => {
   config.values = [1]
-})
+});
 
 extendConfig((config, _userConfig) => {
   config.values.push(2)
-})
+});
 
-module.exports = {}
+module.exports = {};

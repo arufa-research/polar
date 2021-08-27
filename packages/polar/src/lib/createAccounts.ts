c8ceb1da43c88778d059a7c08b65f7078a070cb6
coco-sha <85037852+coco-sha@users.noreply.github.com>
2021-08-27 07:59:04 +0530

    Add create accounts (#9)
    * add create accounts functions
    
    * add tests

---
+++ packages/polar/src/lib/createAccounts.ts
@@ -0,0 +1,30 @@
+const {
+  Secp256k1Pen, pubkeyToAddress, encodeSecp256k1Pubkey
+} = require("secretjs");
+const { Bip39, Random } = require("@iov/crypto");
+
+import { Account } from "../types";
+
+export async function createAccounts (n: number): Promise<Account[]> {
+  const accounts: Account[] = [];
+  for (let i = 0; i < n; ++i) {
+    // Create random address and mnemonic
+    const mnemonic = Bip39.encode(Random.getBytes(16)).toString();
+
+    // This wraps a single keypair and allows for signing.
+    const signingPen = await Secp256k1Pen.fromMnemonic(mnemonic);
+
+    // Get the public key
+    const pubkey = encodeSecp256k1Pubkey(signingPen.pubkey);
+
+    // Get the wallet address
+    const accAddress = pubkeyToAddress(pubkey, 'secret');
+
+    accounts.push({
+      name: "account_" + i.toString(),
+      address: accAddress,
+      mnemonic: mnemonic
+    });
+  }
+  return accounts;
+}
---
+++ packages/polar/test/lib/createAccounts.ts
@@ -0,0 +1,23 @@
+import { assert } from "chai";
+
+import { createAccounts } from "../../src/lib/createAccounts";
+
+describe("Create accounts", () => {
+  it("should create 1 account", async () => {
+    const res = await createAccounts(1);
+
+    assert.isDefined(res[0].address);
+    assert.isDefined(res[0].name);
+    assert.isDefined(res[0].mnemonic);
+  });
+
+  it("should create 3 accounts", async () => {
+    const res = await createAccounts(3);
+
+    for(let i = 0; i <= 2; ++i) {
+      assert.isDefined(res[i].address);
+      assert.isDefined(res[i].name);
+      assert.isDefined(res[i].mnemonic);
+    }
+  });
+});

be0f436cae0d319f77c35aa995d1444a51f3f03e
udit-gulati <uditgulati6@gmail.com>
2021-09-01 00:23:12 +0530

    add fucntion to fetch signingClient

--- packages/polar/src/lib/client.ts
+++ packages/polar/src/lib/client.ts
@@ -1,11 +1,23 @@
 import { info } from "console";
-import { CosmWasmClient } from "secretjs";
+import { CosmWasmClient, SigningCosmWasmClient } from "secretjs";
 
-import { Network } from "../types";
+import { Account, Network } from "../types";
 
 export function getClient (network: Network): CosmWasmClient {
   info(`Creating client for network: ${network.name}`);
   return new CosmWasmClient(
     network.config.endpoint, network.config.seed, network.config.broadCastMode
+  );
+}
+
+export function getSigningClient (network: Network, account: Account): SigningCosmWasmClient {
+  info(`Creating client for network: ${network.name}`);
+  return new SigningCosmWasmClient(
+    network.config.endpoint,
+    account.address,
+    (signBytes) => account.signingPen.sign(signBytes),
+    network.config.seed,
+    undefined,
+    network.config.broadCastMode
   );
 }

c49b2ae8618d6dd127224edee4195a1b16ec587d
udit-gulati <uditgulati6@gmail.com>
2021-09-12 20:27:47 +0530

    add sample script

--- packages/polar/sample-project/scripts/sample-script.js
+++ packages/polar/sample-project/scripts/sample-script.js
@@ -2,14 +2,21 @@ const { Contract, createAccounts } = require("secret-polar");
 
 
 async function run (runtimeEnv) {
-  const masterAccount = await createAccounts(1);
-
   const contract = new Contract('sample-project', runtimeEnv);
   await contract.parseSchema();
 
-  console.log(masterAccount);
-  console.log(contract);
+  const account = runtimeEnv.config.networks.testnet.accounts[0];
+  const deploy_response = await contract.deploy(account);
+  console.log(deploy_response);
 
+  const contract_info = await contract.instantiate({"count": 102}, "deploy test", account);
+  console.log(contract_info);
+
+  const ex_response = await contract.tx.increment(account);
+  console.log(ex_response);
+
+  const response = await contract.query.get_count();
+  console.log(response);
 }
 
 module.exports = { default: run };

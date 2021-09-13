2ffb7953d865828d62feee3576ceb0d4a3b8c221
Coco Sha <hellococo234@gmail.com>
2021-09-13 19:14:35 +0530

    update sample-project

--- packages/polar/sample-project/scripts/sample-script.js
+++ packages/polar/sample-project/scripts/sample-script.js
@@ -1,18 +1,17 @@
-const { Contract, createAccounts } = require("secret-polar");
-
+const { Contract, getAccountByName } = require("secret-polar");
 
 async function run (runtimeEnv) {
+  const contract_owner = getAccountByName("account_0", runtimeEnv);
   const contract = new Contract('sample-project', runtimeEnv);
   await contract.parseSchema();
 
-  const account = runtimeEnv.config.networks.testnet.accounts[0];
-  const deploy_response = await contract.deploy(account);
+  const deploy_response = await contract.deploy(contract_owner.account);
   console.log(deploy_response);
 
-  const contract_info = await contract.instantiate({"count": 102}, "deploy test", account);
+  const contract_info = await contract.instantiate({"count": 102}, "deploy test", contract_owner.account);
   console.log(contract_info);
 
-  const ex_response = await contract.tx.increment(account);
+  const ex_response = await contract.tx.increment(contract_owner.account);
   console.log(ex_response);
 
   const response = await contract.query.get_count();

eadb468606c2ac8b76eb02fcbbec797ae3f69335
hunny98 <42964467+hunny98@users.noreply.github.com>
2021-09-18 01:03:40 +0530

    Update sample-script.js

--- packages/polar/sample-project/scripts/sample-script.js
+++ packages/polar/sample-project/scripts/sample-script.js
@@ -5,13 +5,13 @@ async function run (runtimeEnv) {
   const contract = new Contract('sample-project', runtimeEnv);
   await contract.parseSchema();
 
-  const deploy_response = await contract.deploy(contract_owner.account);
+  const deploy_response = await contract.deploy(contract_owner);
   console.log(deploy_response);
 
-  const contract_info = await contract.instantiate({"count": 102}, "deploy test", contract_owner.account);
+  const contract_info = await contract.instantiate({"count": 102}, "deploy test", contract_owner);
   console.log(contract_info);
 
-  const ex_response = await contract.tx.increment(contract_owner.account);
+  const ex_response = await contract.tx.increment(contract_owner);
   console.log(ex_response);
 
   const response = await contract.query.get_count();

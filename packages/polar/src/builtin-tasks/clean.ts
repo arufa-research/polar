cea953fb974b65368654d70eba350045848431c3
goku <sharma.hunny1998@gmail.com>
2021-09-28 00:55:34 +0530

    Contract name specified test not passing

--- packages/polar/src/builtin-tasks/clean.ts
+++ packages/polar/src/builtin-tasks/clean.ts
@@ -2,12 +2,14 @@ import chalk from "chalk";
 import fsExtra from "fs-extra";
 import path from "path";
 
+import { ERRORS } from "../../src/internal/core/errors-list";
 import { task } from "../internal/core/config/config-env";
+import { PolarError } from "../internal/core/errors";
 import { ARTIFACTS_DIR, isCwdProjectDir } from "../internal/core/project-structure";
 import type { PolarRuntimeEnvironment } from "../types";
 import { TASK_CLEAN } from "./task-names";
 
-interface TaskCleanArg {
+export interface TaskCleanArg {
   contractName: string
 }
 
@@ -22,15 +24,20 @@ export default function (): void {
       { contractName }: TaskCleanArg,
       env: PolarRuntimeEnvironment
     ) => {
+      const comp = './artifacts/contracts/' + contractName + '.wasm';
       if (!isCwdProjectDir()) {
         console.log(`Not in a valid polar project repo, exiting`);
         process.exit(1);
-      } else if (contractName.length !== 0) {
+      } else if (!fsExtra.existsSync(`./${ARTIFACTS_DIR}`)) {
+        throw new PolarError(ERRORS.GENERAL.ARTIFACTS_NOT_FOUND);
+      } else if (contractName.length !== 0 && fsExtra.existsSync(comp)) {
         const artifactsAbsPath = path.resolve(process.cwd(), ARTIFACTS_DIR);
         console.log(`Cleaning Artifacts directory: ${chalk.gray(artifactsAbsPath)}`);
-        await fsExtra.remove('./artifacts/contracts/' + contractName + '.wasm');
+        await fsExtra.remove(comp);
         await fsExtra.remove('./artifacts/schema/' + contractName + '/');
         await fsExtra.remove('./artifacts/checkpoints/' + contractName + '.yaml}');
+      } else if (contractName.length !== 0 && !(fsExtra.existsSync(comp))) {
+        throw new PolarError(ERRORS.GENERAL.INCORRECT_CONTRACT_NAME);
       } else {
         const artifactsAbsPath = path.resolve(process.cwd(), ARTIFACTS_DIR);
         console.log(`Cleaning Artifacts directory: ${chalk.gray(artifactsAbsPath)}`);
---
+++ packages/polar/test/builtin-tasks/clean.ts
@@ -0,0 +1,52 @@
+import { assert } from "chai";
+import fs from "fs-extra";
+
+import contractName from "../../src/builtin-tasks/clean";
+import { TASK_CLEAN } from "../../src/builtin-tasks/task-names";
+import { ERRORS } from "../../src/internal/core/errors-list";
+import { ARTIFACTS_DIR, isCwdProjectDir } from "../../src/internal/core/project-structure";
+import { compile } from "../../src/lib/compile/compile";
+import { useEnvironment } from "../helpers/environment";
+import { expectPolarErrorAsync } from "../helpers/errors";
+import { useFixtureProject } from "../helpers/project";
+
+describe("Clean task", () => {
+  useFixtureProject("testproject");
+  useEnvironment();
+
+  afterEach(() => {
+    fs.removeSync(`./${ARTIFACTS_DIR}`);
+  });
+
+  it("When contract name is not specified", async function () {
+    await compile(false, [], false);
+    await this.env.run(TASK_CLEAN, {});
+
+    assert.isFalse(fs.existsSync(`./${ARTIFACTS_DIR}`));
+  });
+
+  it("When there is no Artifacts directory", async function () {
+    await expectPolarErrorAsync(
+      async () => await this.env.run(TASK_CLEAN, {}),
+      ERRORS.GENERAL.ARTIFACTS_NOT_FOUND
+    );
+  });
+
+  it("When contract name specified is incorrect", async function () {
+    await compile(false, [], false);
+
+    await expectPolarErrorAsync(
+      async () => await this.env.run(TASK_CLEAN, { contractName: "sample-project1" }),
+      ERRORS.GENERAL.INCORRECT_CONTRACT_NAME
+    );
+  });
+
+  it("When contract name is specified", async function () {
+    await compile(false, [], false);
+    await this.env.run(TASK_CLEAN, { contractName: "sample-project" });
+    assert.isTrue(fs.existsSync(`./${ARTIFACTS_DIR}`));
+    assert.isFalse(fs.existsSync(`./${ARTIFACTS_DIR}/contracts/sample-project.wasm`));
+    assert.isFalse(fs.existsSync(`./${ARTIFACTS_DIR}/schema/sample-project`));
+    assert.isFalse(fs.existsSync(`./${ARTIFACTS_DIR}/checkpoints/sample-project.yaml`));
+  });
+});

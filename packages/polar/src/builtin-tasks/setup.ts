d0abd9862bfb1632a02e45fce0b174061989125e
Coco Sha <hellococo234@gmail.com>
2021-09-30 21:02:27 +0530

    add polar install

--- packages/polar/src/builtin-tasks/setup.ts
+++ packages/polar/src/builtin-tasks/setup.ts
@@ -1,9 +1,7 @@
-import chalk from "chalk";
+// eslint-disable-line  @typescript-eslint/restrict-template-expressions
 import { execSync } from "child_process";
 
 import { task } from "../internal/core/config/config-env";
-import { ARTIFACTS_DIR, isCwdProjectDir } from "../internal/core/project-structure";
-import type { PolarRuntimeEnvironment } from "../types";
 import { TASK_SETUP } from "./task-names";
 
 export default function (): void {
@@ -12,14 +10,14 @@ export default function (): void {
 }
 
 async function setupRust (): Promise<boolean> {
-  execSync(`curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh`);
-  // execSync(`export PATH="${HOME}/.cargo/bin:${PATH}"`);
+  execSync(`curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh`); // eslint-disable-line  @typescript-eslint/restrict-template-expressions
+  execSync(`export PATH="${process.env.HOME}/.cargo/bin:${process.env.PATH}"`); // eslint-disable-line  @typescript-eslint/restrict-template-expressions
   execSync(`rustup default stable`);
   execSync(`rustup target list --installed`);
   execSync(`rustup target add wasm32-unknown-unknown`);
   execSync(`rustup install nightly`);
   execSync(`rustup target add wasm32-unknown-unknown --toolchain nightly`);
-  execSync(`sudo apt install build-essential`);
+  if (navigator.userAgent.match(/Linux/i) ?? navigator.userAgent.match(/Windows/i)) { execSync(`sudo apt install build-essential`); }
 
   return true;
 }

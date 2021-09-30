8a9260c05087234e2a8ee4e698df789c113036fc
Coco Sha <hellococo234@gmail.com>
2021-09-30 22:25:11 +0530

    update

--- packages/polar/src/builtin-tasks/install.ts
+++ packages/polar/src/builtin-tasks/install.ts
@@ -9,15 +9,14 @@ export default function (): void {
 }
 
 async function setupRust (): Promise<boolean> {
-  execSync(`curl --proto '=https' --tlsv1.2 -sSf -y https://sh.rustup.rs | sh`);
+  execSync(`curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh`);
   execSync(`export PATH="${process.env.HOME}/.cargo/bin:${process.env.PATH}"`); // eslint-disable-line  @typescript-eslint/restrict-template-expressions
   execSync(`rustup default stable`);
   execSync(`rustup target list --installed`);
   execSync(`rustup target add wasm32-unknown-unknown`);
   execSync(`rustup install nightly`);
   execSync(`rustup target add wasm32-unknown-unknown --toolchain nightly`);
-  console.log(navigator);
-  if (navigator.userAgent.match(/Linux/i) ?? navigator.userAgent.match(/Windows/i)) { execSync(`sudo apt install build-essential`); }
+  if (process.platform === "linux" || process.platform === "win32") { execSync(`sudo apt install build-essential`); }
 
   return true;
 }

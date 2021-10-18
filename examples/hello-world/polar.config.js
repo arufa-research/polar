0d91d17e14c5299bef2baafb03f11f80c4662d81
hunny98 <42964467+hunny98@users.noreply.github.com>
2021-10-18 22:33:54 +0530

    Hello World (#39)
    * readme updated
    
    * Multi-contracts with same name raise error + Doc + Test
    
    * Update packages/polar/test/builtin-tasks/compile.ts
    
    Co-authored-by: coco-sha <85037852+coco-sha@users.noreply.github.com>
    
    * Contract Name not passed
    
    * Contract Name not passed
    
    * Passed contract name
    
    * timeout increased
    
    * hello world
    
    * Added gitignore for target and schema
    
    * removed target and hello world added
    
    * removed target and schema from sample project
    
    * folder names updated and removed artifacts
    
    Co-authored-by: coco-sha <85037852+coco-sha@users.noreply.github.com>

---
+++ examples/hello-world/polar.config.js
@@ -0,0 +1,39 @@
+const accounts = [
+  {
+    name: 'account_0',
+    address: 'secret1l0g5czqw7vjvd20ezlk4x7ndgyn0rx5aumr8gk',
+    mnemonic: 'snack cable erode art lift better october drill hospital clown erase address'
+  },
+  {
+    name: 'account_1',
+    address: 'secret1ddfphwwzqtkp8uhcsc53xdu24y9gks2kug45zv',
+    mnemonic: 'sorry object nation also century glove small tired parrot avocado pulp purchase'
+  }
+];
+
+module.exports = {
+  networks: {
+    default: {
+      endpoint: 'http://localhost:1337/'
+    },
+    development: {
+      endpoint: 'tcp://0.0.0.0:26656',
+      nodeId: '115aa0a629f5d70dd1d464bc7e42799e00f4edae',
+      chainId: 'enigma-pub-testnet-3',
+      keyringBackend: 'test',
+      types: {}
+    },
+    // Holodeck Testnet
+    testnet: {
+      endpoint: 'http://bootstrap.secrettestnet.io',
+      chainId: 'holodeck-2',
+      trustNode: true,
+      keyringBackend: 'test',
+      accounts: accounts,
+      types: {}
+    }
+  },
+  mocha: {
+    timeout: 60000
+  }
+};

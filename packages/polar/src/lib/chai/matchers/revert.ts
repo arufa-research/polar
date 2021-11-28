188bacd613f769d534ed6e7fbbb561a9bfe85f00
udit-gulati <uditgulati6@gmail.com>
2021-11-29 02:20:41 +0530

    add revert assertion

---
+++ packages/polar/src/lib/chai/matchers/revert.ts
@@ -0,0 +1,32 @@
+
+export function supportReverted (Assertion: Chai.AssertionStatic): void {
+  Assertion.addProperty('reverted', function (this: any): any {
+    const promise = this._obj;
+    const onSuccess = (value: any): any => {
+      this.assert(
+        false,
+        'Expected transaction to be reverted',
+        'Expected transaction NOT to be reverted',
+        'Transaction reverted.',
+        'Transaction NOT reverted.'
+      );
+      return value;
+    };
+    const onError = (error: any): any => {
+      const message = (error instanceof Object && 'message' in error) ? error.message : JSON.stringify(error);
+      const isReverted = message.search('failed to execute message') >= 0;
+      this.assert(
+        isReverted,
+        `Expected transaction to be reverted, but other exception was thrown: ${message as string}`,
+        'Expected transaction NOT to be reverted',
+        'Transaction reverted.',
+        error
+      );
+      return error;
+    };
+    const derivedPromise = promise.then(onSuccess, onError);
+    this.then = derivedPromise.then.bind(derivedPromise);
+    this.catch = derivedPromise.catch.bind(derivedPromise);
+    return this;
+  });
+}

3ff54b24aeab41c32f7317e8a55f6e635db35428
udit-gulati <uditgulati6@gmail.com>
2021-12-01 16:57:27 +0530

    add sample test script to sample project

---
+++ packages/polar/sample-project/test/sample-test.js
@@ -0,0 +1,23 @@
+const { expect, use } = require("chai");
+const { Contract, getAccountByName, polarChai } = require("secret-polar");
+
+use(polarChai);
+
+async function run (runtimeEnv) {
+  const contract_owner = getAccountByName("account_1");
+  const other = getAccountByName("account_0");
+  const contract = new Contract("sample-project");
+  await contract.parseSchema();
+
+  const deploy_response = await contract.deploy(contract_owner);
+  console.log(deploy_response);
+
+  const contract_info = await contract.instantiate({"count": 102}, "deploy test", contract_owner);
+  console.log(contract_info);
+
+  await expect(contract.tx.reset(other, [], 100)).to.be.revertedWith("unauthorized");
+
+  await expect(contract.query.get_count()).to.respondWith({ 'count': 100 });
+}
+
+module.exports = { default: run };
